09f39714080101a8406f3de1bdecd80e1de89cd4
bencrow11 <bencrow11@gmail.com>
2023-11-28 15:01:06 +0000

    Added everything

---
+++ src/main/java/org/pokesplash/moveshop/ui/MainUI.java
@@ -0,0 +1,70 @@
+package org.pokesplash.moveshop.ui;
+
+import ca.landonjw.gooeylibs2.api.UIManager;
+import ca.landonjw.gooeylibs2.api.button.Button;
+import ca.landonjw.gooeylibs2.api.button.FlagType;
+import ca.landonjw.gooeylibs2.api.button.GooeyButton;
+import ca.landonjw.gooeylibs2.api.button.PlaceholderButton;
+import ca.landonjw.gooeylibs2.api.helpers.PaginationHelper;
+import ca.landonjw.gooeylibs2.api.page.LinkedPage;
+import ca.landonjw.gooeylibs2.api.page.Page;
+import ca.landonjw.gooeylibs2.api.template.types.ChestTemplate;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.pokesplash.moveshop.MoveShop;
+import org.pokesplash.moveshop.config.CategoryConfig;
+import org.pokesplash.moveshop.util.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainUI {
+	public Page getPage(Pokemon pokemon) {
+		PlaceholderButton placeholderButton = new PlaceholderButton();
+
+		List<Button> categoryButtons = new ArrayList<>();
+		for (CategoryConfig category : MoveShop.config.getCategories()) {
+			Button button = GooeyButton.builder()
+					.display(Utils.parseItemId(category.getDisplayItem()))
+					.title(category.getName())
+					.onClick(el -> {
+						ServerPlayerEntity player = el.getPlayer();
+
+						UIManager.openUIForcefully(player, new CategoryUI().getPage(category, el.getPlayer(), pokemon));
+					})
+					.build();
+
+			categoryButtons.add(button);
+		}
+
+		GooeyButton back = GooeyButton.builder()
+				.display(Utils.parseItemId(MoveShop.lang.getBackButtonMaterial()))
+				.title("§cBack")
+				.lore(new ArrayList<>())
+				.hideFlags(FlagType.All)
+				.onClick(e -> {
+					UIManager.openUIForcefully(e.getPlayer(), new PokemonUI().getPage(e.getPlayer()));
+				})
+				.build();
+
+		GooeyButton filler = GooeyButton.builder()
+			.display(Utils.parseItemId(MoveShop.lang.getFillerMaterial()))
+			.title("")
+			.lore(new ArrayList<>())
+			.hideFlags(FlagType.All)
+			.build();
+
+		int rows = (int) Math.ceil((double) MoveShop.config.getCategories().size() / 7) + 2;
+
+		ChestTemplate template = ChestTemplate.builder(rows)
+				.rectangle(1, 1, rows - 2, 7, placeholderButton)
+				.fill(filler)
+				.set(0, 0, back)
+				.build();
+
+		LinkedPage page = PaginationHelper.createPagesFromPlaceholders(template, categoryButtons, null);
+		page.setTitle(MoveShop.lang.getTitle());
+
+		return page;
+	}
+}
---
+++ src/main/java/org/pokesplash/moveshop/ui/MoveUI.java
@@ -0,0 +1,94 @@
+package org.pokesplash.moveshop.ui;
+
+import ca.landonjw.gooeylibs2.api.UIManager;
+import ca.landonjw.gooeylibs2.api.button.FlagType;
+import ca.landonjw.gooeylibs2.api.button.GooeyButton;
+import ca.landonjw.gooeylibs2.api.page.GooeyPage;
+import ca.landonjw.gooeylibs2.api.page.Page;
+import ca.landonjw.gooeylibs2.api.template.types.ChestTemplate;
+import com.cobblemon.mod.common.api.moves.MoveTemplate;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import net.impactdev.impactor.api.economy.accounts.Account;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.TextColor;
+import org.pokesplash.moveshop.MoveShop;
+import org.pokesplash.moveshop.config.CategoryConfig;
+import org.pokesplash.moveshop.config.MoveConfig;
+import org.pokesplash.moveshop.util.ImpactorUtils;
+import org.pokesplash.moveshop.util.Utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MoveUI {
+	public Page getPage(MoveTemplate move, MoveConfig moveConfig, CategoryConfig category,
+	                    ServerPlayerEntity player, Pokemon pokemon) {
+		Collection<MutableText> lore = new ArrayList<>();
+		lore.add(move.getDescription().setStyle(Style.EMPTY.withColor(TextColor.parse("aqua"))));
+
+		GooeyButton itemButton = GooeyButton.builder()
+				.display(Utils.parseItemId(category.getDisplayItem()))
+				.title(move.getDisplayName())
+				.lore(MutableText.class, lore)
+				.build();
+
+		Collection<String> purchaseLore = new ArrayList<>();
+		purchaseLore.add("§aBuy: §e" + moveConfig.getPrice());
+		purchaseLore.add("§6Current Balance: " + ImpactorUtils.getAccount(player.getUuid()).balanceAsync().join());
+
+		GooeyButton purchase = GooeyButton.builder()
+				.display(Utils.parseItemId(MoveShop.lang.getPurchaseMaterial()))
+				.title("§aPurchase")
+				.lore(purchaseLore)
+				.onClick(e -> {
+					UIManager.openUIForcefully(e.getPlayer(), new TeachUI().getPage(pokemon, player, moveConfig, move));
+				})
+				.build();
+
+		Account account = ImpactorUtils.getAccount(player.getUuid());
+
+		Collection<String> poorLore = new ArrayList<>();
+		poorLore.add("§cYou need " + moveConfig.getPrice() + " to purchase this.");
+		poorLore.add("§cYour current balance is " + account.balanceAsync().join());
+		GooeyButton poor = GooeyButton.builder()
+				.display(Utils.parseItemId(MoveShop.lang.getInsufficientBalanceMaterial()))
+				.title("§cInsufficient Funds")
+				.lore(poorLore)
+				.build();
+
+		GooeyButton cancel = GooeyButton.builder()
+				.display(Utils.parseItemId(MoveShop.lang.getCancelMaterial()))
+				.title("§cCancel")
+				.onClick(e -> {
+					UIManager.openUIForcefully(e.getPlayer(), new CategoryUI().getPage(category, e.getPlayer(),
+							pokemon));
+				})
+				.build();
+
+		GooeyButton filler = GooeyButton.builder()
+				.display(Utils.parseItemId(MoveShop.lang.getFillerMaterial()))
+				.title("")
+				.lore(new ArrayList<>())
+				.hideFlags(FlagType.All)
+				.build();
+
+		ChestTemplate template = ChestTemplate.builder(3)
+				.fill(filler)
+				.set(11, purchase)
+				.set(13, itemButton)
+				.set(15, cancel)
+				.build();
+
+
+		if (account.balanceAsync().join().doubleValue() < moveConfig.getPrice()) {
+			template.set(11, poor);
+		}
+
+		return GooeyPage.builder()
+				.title(move.getDisplayName())
+				.template(template)
+				.build();
+	}
+}
